058ca8a0d665def21aa1390bc4ef9f46f800bf14
= <=>
2024-10-22 02:08:07 -0400

    Update lobby view HEEx

--- lib/join_box_jack_web/live/join/view.ex
+++ lib/join_box_jack_web/live/join/view.ex
@@ -5,48 +5,66 @@ defmodule JoinBoxJackWeb.Join.View do
 
   def render(assigns) do
     ~H"""
-    <p>Hello <%= @player_name %>!
-      Your room code is: <em><%= @room_code %></em></p>
-    <hr />
-    <table id="player-lobby" class="table-fixed boder-separate border-spacing-2">
-      <thead>
-        <tr>
-          <td>Players</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Player Name 1</td>
-        </tr>
-        <tr>
-          <td>Player Name 2</td>
-        </tr>
-        <tr>
-          <td>Player Name 3</td>
-        </tr>
-        <tr>
-          <td>Player Name 4</td>
-        </tr>
-        <tr>
-          <td>Player Name 5</td>
-        </tr>
-        <tr>
-          <td>Player Name 6</td>
-        </tr>
-        <tr>
-          <td>Player Name 7</td>
-        </tr>
-        <tr>
-          <td>Player Name 8</td>
-        </tr>
-        <tr>
-          <td>Player Name 9</td>
-        </tr>
-        <tr>
-          <td>Player Name 10</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <!-- <table id="player-lobby" class="table-fixed boder-separate border-spacing-2"> -->
+      <table
+        id="player-lobby"
+        class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+      >
+        <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+          Your room code: <em><%= @room_code %></em>
+          <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+            Invite others to join with the room code above. Once everybody's in you can organize them into groups.
+          </p>
+        </caption>
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" class="px-6 py-3">Players</th>
+            <th scope="col" class="px-6 py-3">Ready</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              <%= @player_name %>
+            </th>
+            <td class="px-6 py-4">
+              Not Ready
+            </td>
+          </tr>
+          <tr>
+            <td>Player Name 2</td>
+          </tr>
+          <tr>
+            <td>Player Name 3</td>
+          </tr>
+          <tr>
+            <td>Player Name 4</td>
+          </tr>
+          <tr>
+            <td>Player Name 5</td>
+          </tr>
+          <tr>
+            <td>Player Name 6</td>
+          </tr>
+          <tr>
+            <td>Player Name 7</td>
+          </tr>
+          <tr>
+            <td>Player Name 8</td>
+          </tr>
+          <tr>
+            <td>Player Name 9</td>
+          </tr>
+          <tr>
+            <td>Player Name 10</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     """
   end
 
